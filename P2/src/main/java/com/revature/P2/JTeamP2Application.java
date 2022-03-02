d5263d033a4da33c88ca34735f999a5907b3bf2e
James Bowen (J.D.) <j.bowen1821@gmail.com>
2022-03-02 13:57:44 -0600

    added Controllers/Models/Repositories for Chart.js

--- P2/src/main/java/com/revature/P2/JTeamP2Application.java
+++ P2/src/main/java/com/revature/P2/JTeamP2Application.java
@@ -3,7 +3,15 @@ package com.revature.P2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 @SpringBootApplication
+@EnableJpaRepositories("com.revature.repositories")
+@EntityScan("com.revature.models")
+@ComponentScan("com.revature")
+
 public class JTeamP2Application {
 
 	public static void main(String[] args) {
---
+++ P2/src/main/java/com/revature/controllers/LoginController.java
@@ -0,0 +1,46 @@
+package com.revature.controllers;
+
+import com.revature.models.User;
+import com.revature.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServlet;
+
+
+@RestController
+@RequestMapping("/login")
+public class LoginController extends HttpServlet {
+
+    private UserRepository userRepo;
+
+    @Autowired
+    public LoginController(UserRepository userRepo){
+        this.userRepo = userRepo;
+    }
+
+
+    @PostMapping
+    public RedirectView addUser(@RequestParam("password") String password, @RequestParam("email") String email) {
+
+        User u = userRepo.findByEmail(email);
+        RedirectView redirectView = new RedirectView();
+        System.out.println(u);
+
+        redirectView.setUrl("http://localhost:8080/com/");
+
+        if(u!=null){
+            System.out.println(u);
+
+            if (email.equals(u.getEmail()) && password.equals(u.getPassword())) {
+                redirectView.setUrl("home.html");
+            }
+        }
+        return redirectView;
+
+    }
+}
