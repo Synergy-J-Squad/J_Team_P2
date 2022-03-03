5a5a344da803353dfd47a58fd9f34a2d2a86701b
JesusMitchell <97060771+JesusMitchell@users.noreply.github.com>
2022-03-03 10:57:41 -0800

    yt

--- P2/src/main/java/com/revature/repositories/mostpopularRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repositories;
-
-import com.revature.models.mostpopular;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface mostpopularRepository extends JpaRepository<mostpopular,Integer> {
-}
--- P2/src/main/java/com/revature/repositories/topsellingcategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repositories;
-
-import com.revature.models.topsellingcategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface topsellingcategoryRepository extends JpaRepository<topsellingcategory, Integer> {
-}
