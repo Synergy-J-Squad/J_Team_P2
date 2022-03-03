7e90e09af3aae7cf0e5a21de6713424fbadc1b10
JesusMitchell <97060771+JesusMitchell@users.noreply.github.com>
2022-03-03 09:44:05 -0800

    Posted

--- P2/src/main/java/com/revature/repositories/CleanDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repositories;
-
-import com.revature.models.CleanData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CleanDataRepository extends JpaRepository<CleanData, Integer> {
-}
--- P2/src/main/java/com/revature/repositories/highesttrafficRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repositories;
-
-import com.revature.models.highesttraffic;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface highesttrafficRepository extends JpaRepository<highesttraffic,Integer> {
-}
