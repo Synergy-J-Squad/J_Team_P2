791b0dc80171a567ca9fe9734409c70533df37d9
JesusMitchell <97060771+JesusMitchell@users.noreply.github.com>
2022-03-03 09:49:12 -0800

    5 questions

---
+++ P2/src/main/java/com/revature/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.revature.repositories;
+
+import com.revature.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    User findByEmail(String Email);
+
+
+}
